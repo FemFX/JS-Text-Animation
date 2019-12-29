edbfaccde7ccfc23da2b1ba6dff50e72e18c5741
FemFX <ignedko34@gmail.com>
2019-12-29 10:03:21 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,20 @@
+const text = document.querySelector('.text');
+const strText = text.textContent;
+const splitText = strText.split('');
+text.textContent = '';
+for (let i = 0; i < splitText.length; i++) {
+  text.innerHTML += `<span>${splitText[i]}</span>`;
+}
+let char = 0;
+
+const onTick = () => {
+  const span = text.querySelectorAll('span')[char];
+  span.classList.add('fade');
+  char++;
+  if (char == splitText.length) {
+    clearInterval(timer);
+    timer = null;
+    return;
+  }
+};
+let timer = setInterval(onTick, 50);
